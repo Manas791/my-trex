42ef4dd1673a4f07781502cdd6b32ef697f4e4ab
Manas791 <71217267+Manas791@users.noreply.github.com>
2020-10-04 12:32:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,8 +58,8 @@ if (playerscore%100==0&& playerscore>0) {
     
 }
 
-if (touches.length>0||keyDown("space")&& trex.y>=height-500 ) {
- trex.velocityY=-15  
+if (touches.length>0||keyDown("space")&& trex.y>=240 ) {
+ trex.velocityY=-10  
 //playSound("sound://category_explosion/puzzle_game_break_magic_02.mp3");
 touches=[]
 }
